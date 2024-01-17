87e7b408e6ba5bcfd4f59687c6556f37bb2cfd88
Sumin13 <su184913@icloud.com>
2024-01-17 19:16:52 +0000

    Update date.js

--- practice.js/date.js
+++ practice.js/date.js
@@ -1,1 +1,17 @@
-x = x + y;
+//  let x;
+// let d = new Date();
+
+// x = d.toString();
+// x = d.getTime();
+
+// x = d.getMonth() + 1;
+// console.log(x);
+
+// x = `${d.getFullYear()} - ${d.getMonth() + 1} `;
+
+// console.log(x);
+
+const numbers = [12, 3, 34, 7];
+
+const fruits = new Array("apple", "banana");
+console.log(fruits);
