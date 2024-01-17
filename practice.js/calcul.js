3273508624d41656f09e8eeb355ead705b971f3a
Sumin13 <su184913@icloud.com>
2024-01-17 16:14:13 +0000

    Create calcul.js

---
+++ practice.js/calcul.js
@@ -0,0 +1,22 @@
+x = Math.floor(Math.random() * 100 + 1);
+y = Math.floor(Math.random() * 50 + 1);
+
+const sum = x + y;
+const sumOutput = `${x}+${y} = ${sum}`;
+console.log(sumOutput);
+
+const dif = x - y;
+const differenceOutput = `${x}-${y} = ${dif}`;
+console.log(differenceOutput);
+
+const pro = x * y;
+const productOutput = `${x}-${y} = ${pro}`;
+console.log(productOutput);
+
+const quo = x / y;
+const quotientOutput = `${x} / ${y} = ${quo}`;
+console.log(quotientOutput);
+
+const rm = x % y;
+const rmOutput = `${x} % ${y} = ${rm}`;
+console.log(rmOutput);
